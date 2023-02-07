fb883e46971862949a9fa9b6922dce8fa97d5a0a
anil <bhutia.anil@gmail.com>
2023-02-08 01:00:43 +0530

    added code.py and file-br-hydrbad to hydrbad branch

--- code.py
+++ code.py
@@ -1,12 +1,4 @@
-# Program to check if a number is prime or not
-
-num = 29
-
-# To take input from the user
-#num = int(input("Enter a number: "))
-
-# define a flag variable
-flag = False
+e
 
 if num == 1:
     print(num, "is not a prime number")
